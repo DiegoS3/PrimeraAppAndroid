02b1c0a40c2efee5c4e001247ebee021a89d8d1a
DiegoS3 <diegozj92@gmail.com>
2020-10-13 19:45:09 +0200

    Añadida funcionalidad boton email

--- app/src/main/java/android/com/diego/primeraappandroid/MainActivity.kt
+++ app/src/main/java/android/com/diego/primeraappandroid/MainActivity.kt
@@ -1,11 +1,26 @@
 package android.com.diego.primeraappandroid
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainIbtnSendMail.setOnClickListener{ composeEmail()}
+
+    }
+
+    private fun composeEmail() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
