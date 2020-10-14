44230bd17dca27838528cd1ffa5e341e141eb1c7
DiegoS3 <diegozj92@gmail.com>
2020-10-14 23:01:33 +0200

    Salvar Datos con cambios de estado

--- app/src/main/java/android/com/diego/primeraappandroid/AddCitaActivity.kt
+++ app/src/main/java/android/com/diego/primeraappandroid/AddCitaActivity.kt
@@ -4,25 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.CalendarContract
-import android.view.View
+import android.text.Editable
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_cita.*
-import java.lang.Exception
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 class AddCitaActivity : AppCompatActivity() {
+
+    private var titulo : String = ""
+    private var descripcion : String = ""
+    private var localizacion : String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_cita)
 
-        eventoBtnConfirmar.setOnClickListener { addCalendarEvent(eventoEditTextTitulo.text.toString(), eventoEditTextDesc.text.toString()
-                , eventoEditTextLocalizacion.text.toString()) }
+        this.titulo = eventoEditTextTitulo.text.toString()
+        this.descripcion = eventoEditTextLocalizacion.text.toString()
+        this.localizacion = eventoEditTextLocalizacion.text.toString()
+
+        eventoBtnConfirmar.setOnClickListener { addCalendarEvent(titulo, descripcion
+                , localizacion) }
 
     }
 
     private fun addCalendarEvent(title: String, desc: String, location: String) {
+
         val calendarEvent: Calendar = Calendar.getInstance()
         val intent = Intent(Intent.ACTION_EDIT).apply {
         type = "vnd.android.cursor.item/event"
@@ -40,6 +48,39 @@ class AddCitaActivity : AppCompatActivity() {
         }
         else if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("CICLO", "Start")
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+
+
+    // Para salvar el estado por ejemplo es usando un Bundle en el ciclo de vida
+    override fun onSaveInstanceState(outState: Bundle) {
+        // Salvamos en un bundle estas variables o estados de la interfaz
+        outState.run {
+            // Actualizamos los datos o los recogemos de la interfaz
+            putString("TITULO", titulo)
+            putString("DESC", descripcion)
+            putString("LUGAR", localizacion)
+        }
+        // Siempre se llama a la superclase para salvar las cosas
+        super.onSaveInstanceState(outState)
+    }
+
+    // Para recuperar el estado al volver al un estado de ciclo de vida de la Interfaz
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        // Recuperamos en un bundle estas variables o estados de la interfaz
+        super.onRestoreInstanceState(savedInstanceState)
+        // Recuperamos del Bundle
+        savedInstanceState.run {
+            titulo = getString("TITULO").toString()
+            localizacion = getString("LUGAR").toString()
+            descripcion = getString("DESC").toString()
+
         }
     }
 
